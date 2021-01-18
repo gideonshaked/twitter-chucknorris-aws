9fa9d8c80e17d26c23e1bc4bbfab3ac305012ddb
Gideon Shaked <gideonshaked@gmail.com>
2021-01-17 20:34:26 -0500

    Add spacing

--- src/twitter_chucknorris.py
+++ src/twitter_chucknorris.py
@@ -58,6 +58,7 @@ def setup_twitter_api() -> tweepy.API:
     auth.set_access_token(access_token, access_token_secret)
     api = tweepy.API(auth)
     api.verify_credentials()
+    
     return api
 
 
